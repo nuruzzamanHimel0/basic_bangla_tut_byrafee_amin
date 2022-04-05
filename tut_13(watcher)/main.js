8808fa13a669f0b0257b8bce00ab338f04c0126f
nuruzzamanhimel0 <nuruzzaman.himel147@gmail.com>
2022-04-05 22:45:11 +0600

    done

---
+++ tut_13(watcher)/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+  
+  
+    var vu = new Vue({
+        el: "#app",
+        data: {
+            message:'',
+            message2:'',
+            typing: false,
+            gb:'',
+            mb:''
+        },
+        watch:{
+            gb(value){
+                this.mb = value * 1024;
+            },
+            mb(value){
+                this.gb = value / 1024;
+            },
+            message(newmsg, oldmsg){
+                console.log('message');
+                console.log(newmsg);
+                console.log(oldmsg);
+                this.typing = true;
+                setTimeout(()=>{
+                    this.typing = false;
+                },2000);
+            },
+            message2(newmsg, oldmsg){
+                console.log('message2');
+                console.log(newmsg);
+                console.log(oldmsg);
+                this.typing = true;
+                setTimeout(()=>{
+                    this.typing = false;
+                },2000);
+            },
+        }
+       
+       
+        
+    });
+    
+
+    
+
+});
